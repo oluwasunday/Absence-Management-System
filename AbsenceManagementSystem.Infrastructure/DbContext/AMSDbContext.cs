deff93282c75c9c9bd65ac7a6ae26bdc1c0d4b7c
sundayoladejo <oladejo.sunday@ictgiglogistics.onmicrosoft.com>
2024-07-14 22:44:29 +0100

    dbcontext, employee model and packages created

---
+++ AbsenceManagementSystem.Infrastructure/DbContext/AMSDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AbsenceManagementSystem.Core.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbsenceManagementSystem.Infrastructure.DbContext
+{
+    public class AMSDbContext : IdentityDbContext
+    {
+        public AMSDbContext(DbContextOptions<AMSDbContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
